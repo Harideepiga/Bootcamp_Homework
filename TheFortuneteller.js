9594af35881a2a11205d74c162df2eb2f6f0b039
harideepiga <harideepigacs@gmail.com>
2023-04-15 00:32:03 -0400

    Exercise 5

---
+++ TheFortuneteller.js
@@ -0,0 +1,7 @@
+function tellFortune(_numkids, partnerName, geoLocation, jobTitle) {
+    console.log(`You will be a ${jobTitle} in ${geoLocation}, and married to ${partnerName} with ${_numkids} kids.`);
+  }
+  tellFortune(2, "Sam", "New Jersey", "Scientist");
+  tellFortune(1, "Joe", "Florida", "Tester");
+  tellFortune(0, "Allen", "Boston", "data analyst");
+    
